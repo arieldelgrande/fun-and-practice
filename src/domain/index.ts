3f0b9faf5c18f4e15945f15c54d21a1f670d2ca3
arieldelgrande <ariel.delgrande@meltwater.com>
2025-05-30 20:13:17 -0600

    feat: add GetUsers use case to UserController for user retrieval

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,3 +1,5 @@
+export { GetUsers } from "./use-cases/users/getUsers.use-case";
+
 export { UserDataSource } from "./datasource/users.datasource";
 export { UserEntity } from "./entity";
 export { UserRepository } from "./repository/users.repository";
--- src/presentation/users/controllers.ts
+++ src/presentation/users/controllers.ts
@@ -1,13 +1,19 @@
 import { Request, Response } from "express";
-import { UserRepository } from "../../domain";
+import { GetUsers, UserRepository } from "../../domain";
 
 export class UserController {
   constructor(private readonly userRespository: UserRepository) {}
 
+  private handleErrors(err: any, resp: Response) {
+    // if (err instanceof CustomErrors) throw err;
+  }
+
   public getUser = (req: Request, resp: Response) => {
-    resp.status(200).json({
-      ok: true,
-      message: "Hello from user controller",
-    });
+    const pagination = req.body.pagination;
+
+    new GetUsers(this.userRespository)
+      .execute(pagination)
+      .then((data) => resp.status(200).send(data))
+      .catch((err) => this.handleErrors(err, resp));
   };
 }
