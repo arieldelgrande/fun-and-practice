df9edfbb7371c29875160a0f494269dcc4afdac8
arieldelgrande <ariel.delgrande@meltwater.com>
2025-05-31 16:30:39 -0600

    feat: enhance CreateUserDto for improved pagination handling and validation

--- src/domain/validations/type.validations.ts
+++ src/domain/validations/type.validations.ts
@@ -3,25 +3,32 @@ import { CustomErrors } from "../errors/custom-errors.errors";
 
 export function Number() {
   return (target: any, propertyKey: string) => {
-    let value: number;
+    let value: any;
+
+    if (!target.__number_props__) {
+      target.__number_props__ = [];
+    }
+
+    target.__number_props__.push(propertyKey);
 
     const descriptor: PropertyDescriptor = {
       get() {
         return value;
       },
       set(newValue: number) {
-        // console.log(value, propertyKey);
-        // if (value !== undefined && typeof value !== "number") {
-        //   return value;
-        // }
-        console.log(newValue, propertyKey);
         if (typeof newValue !== "number") {
-          throw CustomErrors.BadRequest(
-            `${propertyKey} must be of type number`,
-          );
+          return (value = {
+            error: true,
+            message: `${propertyKey} must be of type number`,
+            value: null,
+          });
         }
-        value = newValue;
-        return value;
+
+        return (value = {
+          error: false,
+          message: "Value accepted",
+          value: newValue,
+        });
       },
     };
     Object.defineProperty(target, propertyKey, descriptor);
