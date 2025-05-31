27e2a9eb52c5500d0fe44028c3d4ef9142ce415e
arieldelgrande <ariel.delgrande@meltwater.com>
2025-05-30 19:34:45 -0600

    feat: integrate UserRepository into UserController and UserRouter

--- src/presentation/users/controllers.ts
+++ src/presentation/users/controllers.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
+import { UserRepository } from "../../domain";
 
 export class UserController {
+  constructor(private readonly userRespository: UserRepository) {}
+
   public getUser = (req: Request, resp: Response) => {
     resp.status(200).json({
       ok: true,
--- src/presentation/users/router.ts
+++ src/presentation/users/router.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { UserController } from "./controllers";
+import { UserDataSourceImp } from "../../infrastructure/datasource/users.datasource.imp";
+import { UserRepositoryImp } from "../../infrastructure/repository/users.repository.imp";
 
 export class UserRouter {
   static get router() {
     const router = Router();
 
-    const userController = new UserController();
+    const userDataSource = new UserDataSourceImp();
+    const userRepository = new UserRepositoryImp(userDataSource);
+    const userController = new UserController(userRepository);
 
     router.get("/", userController.getUser);
 
